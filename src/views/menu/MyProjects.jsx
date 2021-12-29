e6c4da835ed714bb4f722814a50a75c6d177a6a2
FabricioCaldana <fanelli@grupodimed.com.br>
2021-12-29 18:55:47 -0300

    alterando projects

--- src/views/menu/MyProjects.jsx
+++ src/views/menu/MyProjects.jsx
@@ -4,8 +4,8 @@ const MyProjects = () => (
   <div className="MyProjects">
     <h1>Meus Projetos</h1>
     <p>
-      Você pode acompanhar alguns dos meus projetos pessoais no GitHub, basta
-      procurar NcryScooby em <a href="https://github.com/" target="_blank" rel="noreferrer">GitHub.com</a> ou acessar por este <a href="https://github.com/NcryScooby" target="_blank" rel="noreferrer">link</a>.
+      Você pode acompanhar alguns dos meus projetos pessoais no github, basta
+      procurar NcryScooby em <a href="https://github.com/" target="_blank" rel="noreferrer">github.com</a> ou acessar por este <a href="https://github.com/NcryScooby" target="_blank" rel="noreferrer">link</a>.
     </p>
   </div>
 );
